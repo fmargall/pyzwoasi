a00b57d5e7b95f7d60887aa1ef0cde645bfeb3be
François Margall <fr.margall@proton.me>
2025-03-04 12:13:03 +0100

    Adding functions at module initialisation

--- pyzwoasi/__init__.py
+++ pyzwoasi/__init__.py
@@ -1,1 +1,28 @@
 from .__version__ import __version__
+
+from.pyzwoasi import (
+    CameraInfo, ControlCaps, ID, SN,
+    getNumOfConnectedCameras,
+    getProductIDs,
+    cameraCheck,
+    getCameraProperty,
+    getCameraPropertyByID,
+    openCamera,
+    initCamera,
+    closeCamera,
+    getNumOfControls,
+    getControlCaps,
+    getControlValue,
+    setControlValue,
+    getROIFormat,
+    setROIFormat,
+    getStartPos,
+    setStartPos,
+    getDroppedFrames,
+    disableDarkSubstract,
+    startVideoCapture,
+    stopVideoCapture,
+    getVideoData,
+    getSDKVersion,
+    getSerialNumber
+    )
